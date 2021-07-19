5f871e2d8f3e660682b68cf60fc44f28ac16e6e8
jc8080 <jc8080@rit.edu>
2021-07-19 17:27:57 -0400

    stubbed out CRUD methods for query

---
+++ Query.py
@@ -0,0 +1,58 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="reddwarf.cs.rit.edu",
+    database="",
+    user="",
+    password=""
+)
+
+cur = conn.cursor()
+
+
+def create_recipe():
+    pass
+
+
+def update_recipe():
+    pass
+
+
+def delete_recipe():
+    pass
+
+
+def create_user():
+    pass
+
+
+def update_user():
+    pass
+
+
+def delete_user():
+    pass
+
+
+def create_ingredient():
+    pass
+
+
+def delete_ingredient():
+    pass
+
+
+def create_category():
+    pass
+
+
+def delete_category():
+    pass
+
+
+def create_item():
+    pass
+
+
+def delete_item():
+    pass
